6c7596e58ed596a03c30a89b859a0215fa33136f
jason-bohlinger <j.bohlinger3@gmail.com>
2024-03-06 10:27:24 -0800

    Added check for win condition

--- board.py
+++ board.py
@@ -1,12 +1,16 @@
-sudokuGrid = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+import numpy as np
+
+sudokuGrid = [[6, 3, 9, 5, 7, 4, 1, 8, 2],
+              [5, 4, 1, 8, 2, 9, 3, 7, 6],
+              [7, 8, 2, 6, 1, 3, 9, 5, 4],
+              [1, 9, 8, 4, 6, 7, 5, 2, 3],
+              [3, 6, 5, 9, 8, 2, 4, 1, 7],
+              [4, 2, 7, 1, 3, 5, 8, 6, 9],
+              [9, 5, 6, 7, 4, 8, 2, 3, 1],
+              [8, 1, 3, 2, 9, 6, 7, 4, 5],
+              [2, 7, 4, 3, 5, 1, 6, 9, 8]]
+def getCol(i):
+    return [row[i] for row in sudokuGrid]
 
 def printGrid():
     print()
@@ -33,6 +37,29 @@ def getGridFromUser():
         for j in range(9):
             sudokuGrid[i][j] = int(rowStr[j])
 
+validRowCol = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+def isWinCondition():
+    ## Check Rows
+    for row in sudokuGrid:
+        if sorted(row) != validRowCol:
+            return False
+    
+    ## Check Cols
+    for i in range(9):
+        if (sorted(getCol(i)) != validRowCol):
+            return False
+        
+    ## Check 3x3s
+    for row in range(0, 9, 3):
+        for column in range(0, 9, 3):
+            line = (sudokuGrid[row][column:column+3]
+                + sudokuGrid[row+1][column:column+3]
+                + sudokuGrid[row+2][column:column+3])
+            if sorted(line) != validRowCol:
+                return False
+    return True
+
+
+
 printGrid()
-getGridFromUser()
-printGrid()
+print(isWinCondition())
